9ea490d6b9a03d9bb3569298e1b3f00eb9b26abd
chao <dc10101@qq.com>
2017-02-23 16:31:16 +0800

    more functions

--- index.js
+++ index.js
@@ -1,20 +1,39 @@
 const _ = require('lodash')
 
-exports.has = (...requiredFields) => {
+function check(para, requiredFields) {
   const checkFunc = (req) => {
-    let params = _.extend({}, req.query, req.body, req.params)
     for(let i = 0; i < requiredFields.length; i++){
-      if(!_.has(params, requiredFields[i])){
+      if(!_.has(para, requiredFields[i])){
         let err = new Error(`Missing parameter: ${requiredFields[i]}`)
         err.status = 400
         return err
       }
     }
-    req.params = params
+    req.para = para
   }
 
   return (req, res, next) => {
     let err = checkFunc(req)
     next(err)
   }
+}
+
+exports.has = (...requiredFields) => {
+  let para = _.extend({}, req.query, req.body, req.params)
+  check(para, requiredFields)
+}
+
+exports.QueryHas = (...requiredFields) => {
+  let para = req.query
+  check(para, requiredFields)
+}
+
+exports.BodyHas = (...requiredFields) => {
+  let para = req.body
+  check(para, requiredFields)
+}
+
+exports.ParamHas = (...requiredFields) => {
+  let para = req.params
+  check(para, requiredFields)
 }
