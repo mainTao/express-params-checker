26ff2d473cf2533ae977a06a47675ebc0d5d9bd9
chao <dc10101@qq.com>
2017-02-24 11:09:57 +0800

    rename req.params to req.para

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ const _ = require('lodash')
 
 exports.has = (...requiredFields) => {
   const checkFunc = (req) => {
-    let params = _.extend({}, req.query, req.body, req.params)
+    let para = _.extend({}, req.query, req.body, req.params)
     for(let i = 0; i < requiredFields.length; i++){
-      if(!_.has(params, requiredFields[i])){
+      if(!_.has(para, requiredFields[i])){
         let err = new Error(`Missing parameter: ${requiredFields[i]}`)
         err.status = 400
         return err
       }
     }
-    req.params = params
+    req.para = para
   }
 
   return (req, res, next) => {
